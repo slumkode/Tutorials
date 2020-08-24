3483754673f504db64aef133a5ce435a2ef00f69
slumkode <slumkode@gmail.com>
2020-08-24 17:34:24 +0300

    Templating  101 - Passing dynamic data to views (part 2)
    introduction to view_cell

--- introduction/app/Views/blog.php
+++ introduction/app/Views/blog.php
@@ -1,12 +1,8 @@
 
 <h1><?= $title ?></h1>
 
-<div>
+<div class='row'> 
     <?php foreach($posts as $post): ?>
-        <div>
-            <h3><?= $post; ?></h3>
-            <img src="<?= base_url('assets/images/sample.jpeg') ?>" style="width: 200px; height: auto;" alt="sample">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero perferendis esse, quas atque ipsum obcaecati dolorum quod sed numquam commodi laboriosam non vitae maiores tempore recusandae laborum, ratione veniam modi?</p>
-        </div>
+        <?= view_cell('\App\Libraries\Blog::postItem', ['title' => $post]); ?> 
     <?php endforeach; ?>
 </div>
---
+++ introduction/app/Views/components/post_item.php
@@ -0,0 +1,8 @@
+<div class="card col-12 col-sm-6 col-md-4" style="width: 18rem;">
+    <img src="<?= base_url('assets/images/sample.jpeg') ?>" class="card-img-top" alt="blog image">
+    <div class="card-body">
+        <h5 class="card-title"><?= $title; ?></h5>
+        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi repudiandae adipisci tenetur ut aut! Officiis tenetur totam necessitatibus, labore aperiam optio illo quibusdam architecto eveniet rem. Pariatur nemo sunt esse?</p>
+        <a href="<?= base_url('blog/post')?>" class="btn btn-primary">Read more</a>
+    </div>
+</div>
