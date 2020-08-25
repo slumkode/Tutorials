66c959b90c1357ebb7ad084c13e54f88df6f84bf
slumkode <slumkode@gmail.com>
2020-08-25 12:21:04 +0300

    Templating  101 - Passing dynamic data to views - Layouts (part 3 b)

--- introduction/app/Views/single_post.php
+++ introduction/app/Views/single_post.php
@@ -1,6 +1,14 @@
 <?= $this->extend('layouts/main') ?>
 
 <?= $this->section('content') ?>
-    <h1><?= $title ?></h1>
+<h1><?= $title ?></h1>
+
+
+<div class="row">
+    <?= $this->include('partials/sidebar')?>
+    <div class="col-12 col-sm-9">
+        single post content goes here
+    </div>
+</div>
 
 <?= $this->endSection() ?>
